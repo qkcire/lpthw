5cd4ea2e5f5a4ad99f68a62f489afeb2e9adf79c
Erick Quintanilla <erickbquintanilla@gmail.com>
2019-01-25 11:52:04 -0800

    Complete study drills 5

--- exercises/ex5.py
+++ exercises/ex5.py
@@ -1,17 +1,26 @@
-my_name = "Erick B. Quintanilla"
-my_age = 28
-my_height = 70
-my_weight = 179
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = "Black"
+# Study Drills
+# 1. “Change all the variables so there is no my_ in 
+#     front of each one. Make sure you change the name 
+#     everywhere, not just where you used = to set them.”
+# 2. “Try to write some variables that convert the inches
+#     and pounds to centimeters and kilograms. Do not just 
+#     type in the measurements. Work out the math in Python.”
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
-print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+name = "Erick B. Quintanilla"
+age = 28
+height = round(70 / 12, 1)
+weight = round(179 / 2.205, 1)
+eyes = 'Brown'
+teeth = 'White'
+hair = "Black"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} feet tall.")
+print(f"He's {weight} kg heavy.")
+print("(Actually, that's not too heavy.)")
+print(f"He's got {eyes} eyes and {hair} hair.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
